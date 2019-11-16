b4a971115b26e91369c3ed2ec257d44e495360ef
Emil Laine <laine.emil@gmail.com>
2019-11-16 16:36:57 +0200

    Convert block to selected block type

--- src/Block.tsx
+++ src/Block.tsx
@@ -2,11 +2,25 @@ import * as React from "react";
 import {TextField} from "./TextField";
 import {useState} from "./hooks";
 
+export type BlockType = "prompt" | "print" | "empty";
+
 export function Block() {
+  const type = useState<BlockType>("empty");
   const value = useState("");
-  const completions = ["prompt", "print"];
+  const completions: Array<BlockType> = ["prompt", "print"];
+
+  function BlockContent() {
+    switch (type.get()) {
+      case "empty":
+        return <TextField value={value} completions={completions} select={type.set}/>;
+      case "prompt":
+        return <>prompt</>;
+      default:
+        return <>{type.get()}</>;
+    }
+  }
 
   return <div className="Block">
-    <TextField value={value} completions={completions}/>
+    <BlockContent/>
   </div>;
 }
--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,12 +1,13 @@
 import {Lens} from "./lens";
 import * as React from "react";
 
-type Props = {
+type Props<T> = {
   value: Lens<string>
-  completions?: Array<string>
+  completions?: Array<T>
+  select: (value: T) => void
 }
 
-export function TextField(props: Props) {
+export function TextField<T extends string>(props: Props<T>) {
   return <div className="TextField">
     <input
       value={props.value.get()}
@@ -14,7 +15,7 @@ export function TextField(props: Props) {
     />
     <div className="completions">
       {props.value.get() && props.completions?.filter(c => c.includes(props.value.get())).map(c =>
-        <a href="#" className="completion" onClick={() => props.value.set(c)}>
+        <a href="#" className="completion" onClick={() => props.select(c)} key={c}>
           {c}
         </a>
       )}
