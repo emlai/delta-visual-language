4d6ac9f1e02520f7ba79a607bfe537e076779324
Emil Laine <laine.emil@gmail.com>
2019-11-16 23:11:55 +0200

    Add editor menu

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,15 +1,28 @@
 import * as React from "react";
 import {IoMdPlay} from "react-icons/io";
+import {useKey} from "react-use";
 import {TitleBar} from "electron-react-titlebar";
 import {Block, BlockData} from "./Block";
 import {useState} from "./hooks";
 import {interpret} from "./interpreter";
+import {Position} from "./utils";
+import {Menu} from "./Menu";
 
 export function Editor() {
+  const [editorMenuPosition, setEditorMenuPosition] = React.useState<Position | null>(null);
+  useKey("Escape", () => setEditorMenuPosition(null));
   const blocks = useState<Array<BlockData>>([{type: "empty"}]);
   const setBlock = (index: number, data: BlockData) => {
     blocks.set(Object.assign([], blocks.get().concat({type: "empty"}), {[index]: data}));
   };
+
+  function openEditorMenu(event: React.MouseEvent) {
+    if (!editorMenuPosition && event.target === event.currentTarget) {
+      setEditorMenuPosition({x: event.clientX, y: event.clientY});
+    } else {
+      setEditorMenuPosition(null);
+    }
+  }
 
   return <div className="Editor">
     <TitleBar menu={[]}>
@@ -18,10 +31,16 @@ export function Editor() {
         <IoMdPlay/>
       </div>
     </TitleBar>
-    <div className="editableArea">
+    <div className="editableArea" onClick={openEditorMenu}>
       {blocks.get().map((block, index) =>
         <Block data={block} setData={data => setBlock(index, data)} key={index}/>
       )}
     </div>
+    {editorMenuPosition ?
+      <Menu
+        items={["Add function"]}
+        select={(x) => console.log(x)}
+        position={editorMenuPosition}
+      /> : null}
   </div>;
 }
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
+import {Position} from "./utils";
 
 type Props<T> = {
   items: T[]
   select: (item: T) => void
+  position?: Position
 };
 
 export function Menu<T extends string>(props: Props<T>) {
-  return <div className="Menu">
+  return <div className="Menu" style={props.position && {left: props.position.x, top: props.position.y}}>
     {props.items.map(item =>
       <a href="#" className="MenuItem" onClick={() => props.select(item)} key={item}>
         {item}
